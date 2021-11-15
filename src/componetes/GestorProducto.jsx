fc545f10914abf7f005c9f215a72b6953543cba2
cyberAstronaut <anderson.mirandaf@gmail.com>
2021-11-15 11:13:04 -0500

    Gestor de productos HU_012

---
+++ src/componetes/GestorProducto.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Button, Container} from 'react-bootstrap'
+
+const GestorVenta = () => {
+    return (
+        <div>
+            <Container>
+                <h2>Bienvenido al gestor de Productos</h2>
+                <Button href="/Compra.jsx" variant="success" size="lg">
+                    Nuevo Producto
+                </Button>{' '}
+                <Button variant="secondary" size="lg">
+                    Registros de productos.
+                </Button>
+            </Container>
+        </div>
+    )
+}
+
+export default GestorProducto
---
+++ src/componetes/Producto.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Container, FloatingLabel, Form, Row, Col, Button } from 'react-bootstrap';
+
+const venta = () => {
+    return (
+        <div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col>
+                    <>
+                        <h1 className="text-center">Registro De Productos</h1>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nombre del administrador"
+                        className="mb-3"
+                        >
+                        <Form.Control  placeholder="Dijite su nombre" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nombre del producto"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Dijite el nombre del producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Lote"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Dijite el lote del producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Cantidad"
+                        className="mb-3"
+                        >
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Describa observaciones si hay lugar"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Observaciones" />
+                        </FloatingLabel>  
+                        <Form.Control placeholder="Cantidad" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Precio del Producto"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Precio del Producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Precio Total"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Precio Total" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nombre del producto"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Dijite el nombre del producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        >
+                        <Button variant="primary my-3" type="submit">
+                            Registrar venta
+                        </Button>
+                        </FloatingLabel>
+                    </>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default producto
