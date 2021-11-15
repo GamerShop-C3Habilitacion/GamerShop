f1acfc2f448fa3f5a453ac21aab2a7709f2ed84c
Jhonathan Alejandro <joaleanco@hotmail.com>
2021-11-15 00:16:38 -0500

    creamos componente de registro de venta.

---
+++ src/componetes/Venta.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Container, FloatingLabel, Form, Row, Col, Button } from 'react-bootstrap';
+
+const venta = () => {
+    return (
+        <div>
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col>
+                    <>
+                        <h1 className="text-center">Registro De Ventas</h1>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nombre del Cliente"
+                        className="mb-3"
+                        >
+                        <Form.Control  placeholder="Nombre del Cliente" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Producto"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Cantidad"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Cantidad" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Precio del Producto"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Precio del Producto" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Precio Total"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Precio Total" />
+                        </FloatingLabel>
+                        <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nombre del Vendedor"
+                        className="mb-3"
+                        >
+                        <Form.Control placeholder="Nombre del Vendedor" />
+                        <Button variant="primary my-3" type="submit">
+                            Registrar venta
+                        </Button>
+                        </FloatingLabel>
+                    </>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default venta
