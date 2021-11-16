5b5ea725eb7c7ffd70a7c19faf65fd0c37a5c149
Jhonathan Alejandro <joaleanco@hotmail.com>
2021-11-15 23:17:46 -0500

    agregue componente de gestor de producto

--- src/pages/Administrador.jsx
+++ src/pages/Administrador.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import GestorProducto from '../componetes/GestorProducto'
 import GestorVenta from '../componetes/GestorVenta'
 
 const Administrador = () => {
     return (
         <div>
             <GestorVenta />
+            <GestorProducto />
         </div>
     )
 }
