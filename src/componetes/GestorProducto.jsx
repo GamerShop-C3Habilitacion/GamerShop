f9f6359601586295cf8622fe07092e559c7d371f
Jhonathan Alejandro <joaleanco@hotmail.com>
2021-11-15 23:12:59 -0500

    corregio nombre de la funcion

--- src/componetes/GestorProducto.jsx
+++ src/componetes/GestorProducto.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button, Container} from 'react-bootstrap'
 
-const GestorVenta = () => {
+const GestorProducto = () => {
     return (
         <div>
             <Container>
